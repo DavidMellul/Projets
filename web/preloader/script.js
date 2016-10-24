7a83f7a374ad723da8801477f6ba596d5b656d7e
David Mellul <david.mellul@outlook.fr>
2016-10-24 16:09:27 +0200

    Add files via upload

---
+++ web/preloader/script.js
@@ -0,0 +1,5 @@
+
+setTimeout(function() { document.getElementById("Main").className += "animated slideInUp" ;
+                        document.getElementById("Main").style.opacity = "1"; },3000);
+setTimeout(function() { document.getElementById("Preloader").className += "animated slideOutUp" },3100);
+
