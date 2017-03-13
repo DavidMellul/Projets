340e9b394eea079e34724b6e153a519e42d6a4b9
David Mellul <david.mellul@outlook.fr>
2017-03-14 00:01:29 +0100

    Create Main.java

---
+++ java/Remote/Serveur/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by David on 13/03/2017.
+ */
+@SuppressWarnings("unused")
+public class Main {
+    public static void main(String args[]) throws IOException {
+    	new MainInterface().setVisible(true);
+    }
+}
